3ea093bdf89d9856cf99b4a5711e9d1961cfc116
JongKuk Seo <jongkuk.dev@gmail.com>
2022-05-19 18:10:25 +0900

    Feat: msg 프로젝트 생성

---
+++ msg/src/main/java/com/project/msg/MsgApplication.java
@@ -0,0 +1,13 @@
+package com.project.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgApplication.class, args);
+	}
+
+}
