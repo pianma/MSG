2c1b011b6cb078f4c4a0cd82c275808a5f932101
Pianma <ikkw2040@naver.com>
2022-05-26 18:53:51 +0900

    service컨트롤러

--- msg/src/main/java/com/project/msg/controller/MsgController.java
+++ msg/src/main/java/com/project/msg/controller/MsgController.java
@@ -69,4 +69,49 @@ public class MsgController {
 
         return "controller"; //나중에 파일명앞에 키워드 붙여서 파일이름이 정해져야함.
     }
+
+
+
+    @GetMapping("scon")
+    public String ServiceController(Model model,
+                                    @RequestParam String keyword,
+                                    @RequestParam(required = false) String path,
+                                    @RequestParam(required = false) String primaryKeyField,
+                                    @RequestParam(required = false) String field) {
+
+
+        String keywordName = keyword.toLowerCase().trim();
+        String upperKeyword = keywordName.toUpperCase();
+        String firstLetterToUpper = keywordName.substring(0, 1).toUpperCase() + keywordName.substring(1);
+
+        // 테이블 명 - 키워드
+        String tableName = keywordName.toUpperCase();
+
+        //필드정보 DTO : 필드 타입 및 변수명  ("-" 필드 구분)
+
+        List<FieldDto> fieldDtoList = new ArrayList<FieldDto>();
+        FieldDto primaryFieldDto = null;
+
+        if (field != null && primaryKeyField != null) {
+
+            //기본키 또는 유니크 키 필드 이름
+            primaryFieldDto = msgService.getPrimaryFieldDto(primaryKeyField);
+            log.info("primaryFieldDto = " + primaryFieldDto);
+
+            //필드 리스트 생성(유니크키 변수이름, 필드이름)
+            fieldDtoList.addAll(msgService.getFieldDtoList(primaryFieldDto.getName(), field));
+
+            log.info("fieldDtoList = " + fieldDtoList);
+
+        }
+
+        model.addAttribute("basicPath", path);
+        model.addAttribute("upperKeyword", firstLetterToUpper);
+        model.addAttribute("keyword", keywordName);
+        model.addAttribute("primaryFieldDto", primaryFieldDto);
+        model.addAttribute("primaryFieldDtoNameWithBraces", "{" + primaryFieldDto.getName() + "}");
+
+
+        return "service";
+    }
 }
